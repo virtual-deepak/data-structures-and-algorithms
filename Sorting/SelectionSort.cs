3f78af8fef4823f8e63fde5527a86ec113765743
Deepak Agarwal <deep.agarwal@hotmail.com>
2022-10-30 02:14:26 +0530

    Refactored the code

--- Sorting/SelectionSort.cs
+++ Sorting/SelectionSort.cs
@@ -1,55 +1,42 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.AccessControl;
-
-class Program
+public class SelectionSort
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine(value: "Enter the numbers to be sorted (type 'done' when completed): ");
-        List<int> arrInput = new List<int>();
+    private List<int> numbersToSort;
 
-        string input = Console.ReadLine();
-        while (!string.Equals(input, "done", StringComparison.InvariantCultureIgnoreCase))
-        {
-            if (int.TryParse(input, out int number))
-            {
-                arrInput.Add(number);
-            }
-            else
-            {
-                Console.WriteLine("Not a number, try again.");
-            }
-            input = Console.ReadLine();
-        }
-        SelectionSort(arrInput);
-        Console.WriteLine($"Sorted array: {string.Join(" ", arrInput)}");
+    public SelectionSort(List<int> numbersToSort)
+    {
+        this.numbersToSort = numbersToSort;
     }
 
-    static void SelectionSort(List<int> arrInput)
+    /// <summary>
+    /// Sorts the input numbers
+    /// </summary>
+    public List<int> Sort()
     {
-        for (int index = 0; index < arrInput.Count; index++)
+        for (int index = 0; index < numbersToSort.Count; index++)
         {
-            var minNumber = arrInput[index];
             var minNumberIndex = index;
-
-            for (int innerIndex = index + 1; innerIndex < arrInput.Count; innerIndex++)
+            for (int innerIndex = index + 1; innerIndex < numbersToSort.Count; innerIndex++)
             {
-                if (arrInput[innerIndex] < minNumber)
-                {
-                    minNumber = arrInput[innerIndex];
+                if (numbersToSort[innerIndex] < numbersToSort[minNumberIndex])
                     minNumberIndex = innerIndex;
-                }
             }
-            Swap(arrInput, index, minNumberIndex);
+            Swap(numbersToSort, index, minNumberIndex);
         }
+        return numbersToSort;
     }
 
-    static void Swap(List<int> arrInput, int index, int minNumberIndex)
+    /// <summary>
+    /// Swaps the numbers in the array among the indices provided
+    /// </summary>
+    /// <param name="arrInput"></param>
+    /// <param name="index"></param>
+    /// <param name="minNumberIndex"></param>
+    static void Swap(List<int> numbersToSort, int index, int minNumberIndex)
     {
-        var temp = arrInput[index];
-        arrInput[index] = arrInput[minNumberIndex];
-        arrInput[minNumberIndex] = temp;
+        if (index == minNumberIndex)
+            return;
+        numbersToSort[index] = numbersToSort[index] + numbersToSort[minNumberIndex];
+        numbersToSort[minNumberIndex] = numbersToSort[index] - numbersToSort[minNumberIndex];
+        numbersToSort[index] = numbersToSort[index] - numbersToSort[minNumberIndex];
     }
 }
